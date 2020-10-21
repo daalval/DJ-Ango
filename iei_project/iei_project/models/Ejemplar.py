85116fe8b1b2fc4ea3744a6b66206c5dc61e4959
cargalau <cargalau@inf.upv.es>
2020-10-21 12:42:25 +0200

    Getters

--- iei_project/iei_project/models/Ejemplar.py
+++ iei_project/iei_project/models/Ejemplar.py
@@ -3,3 +3,12 @@ class Ejemplar(object):
         self._volumen = volumen
         self._numero = numero
         self._mes = mes
+    
+    def get_volumen(self):
+        return self._volumen
+
+    def get_numero(self):
+        return self._numero
+
+    def get_mes(self):
+        return self._mes
