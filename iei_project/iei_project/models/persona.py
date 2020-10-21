4c81717af7aa4674f8ddf630dff6aed1b23bb0e7
daalval <daalval@protonmail.com>
2020-10-21 12:20:05 +0200

    creadas las clases Persona y Publicacion

---
+++ iei_project/iei_project/models/persona.py
@@ -0,0 +1,7 @@
+class Persona(object):
+
+    def __init__(self, nombre, apellidos, publicaciones):
+        self.nombre = nombre
+        self.apellidos = apellidos
+        self.publicaciones = publicaciones
+    
