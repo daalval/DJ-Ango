8987124ff075661fd23aa7859730ffb9527ecf7a
daalval <daalval@inf.upv.es>
2020-11-21 18:20:53 +0100

    mas cosas

--- bibliosearch/bbdd/models.py
+++ bibliosearch/bbdd/models.py
@@ -1,46 +1,47 @@
 from django.db import models
-from django.db.models import constraints
 
 # Create your models here.
 
-class Revista(models.model):
-    idRevista = models.IntegerField(primary_key = True, null = False)
-    nombre = models.CharField(null = True)
+class Revista(models.Model):
+    id_revista = models.IntegerField(primary_key = True, null = False)
+    nombre = models.CharField(null = True, max_length = 45)
 
-class Ejemplar(models.model):
-    idEjemplar = models.IntegerField(primary_key = True, null = False)
+class Ejemplar(models.Model):
+    id_ejemplar = models.IntegerField(primary_key = True, null = False)
     volumen = models.IntegerField(null = True)
     numero = models.IntegerField(null = True)
     mes = models.IntegerField(null = True)
-    revista_idRevista = models.IntegerField(null = False)
-    fk_Ejemplar_Revista1_idx = models.Index(revista_idRevista)
-    fk_Ejemplar_Revista1 = models.ForeignKey(Revista, on_delete = models.CASCADE)
+    revista = models.ForeignKey(Revista, on_delete = models.CASCADE)
 
-class Publicacion(models.model):
-    idPublicacion = models.IntegerField(primary_key = True, null = False)
-    titulo = models.CharField(null = True)
+class Publicacion(models.Model):
+    id_publicacion = models.IntegerField(primary_key = True, null = False)
+    titulo = models.CharField(null = True, max_length = 45)
     anyo = anyo = models.IntegerField(null = True)
-    URL = models.CharField(null = True)
+    URL = models.CharField(null = True, max_length = 45)
 
-class Articulo(models.model):
+class Articulo(models.Model):
     pagina_inicio = models.IntegerField(null = True)
     pagina_fin = models.IntegerField(null = True)
-    ejemplar_idEjemplar = models.IntegerField(null = False)
-    publicacion_idPublicacion = models.IntegerField(null = False)
-    fk_Articulo_Ejemplar1_idx = models.Index(ejemplar_idEjemplar)
-    fk_Articulo_Publicacion1_idx = models.Index(publicacion_idPublicacion)
-    fk_Articulo_Ejemplar1 = models.ForeignKey(Ejemplar, on_delete = models.CASCADE)
-    fk_Articulo_Publicacion1 = models.ForeignKey(Publicacion, on_delete = models.CASCADE)
+    ejemplar = models.ForeignKey(Ejemplar, on_delete = models.CASCADE, null = False)
+    publicacion = models.OneToOneField(Publicacion, on_delete = models.CASCADE, null = False, primary_key = True)
 
-class Com_con(models.model):
-    congreso = models.CharField(null = True)
-    edicion = models.CharField(null = True)
-    lugar = models.CharField(null = True)
+class Com_con(models.Model):
+    congreso = models.CharField(null = True, max_length = 45)
+    edicion = models.CharField(null = True, max_length = 45)
+    lugar = models.CharField(null = True, max_length = 45)
     pagina_inicio = models.IntegerField(null = True)
     pagina_fin = models.IntegerField(null = True)
-    publicacion_idPublicacion = models.IntegerField(null = False)
-    fk_Articulo_Publicacion1_idx = models.Index(publicacion_idPublicacion)
-    fk_Articulo_Publicacion1 = models.ForeignKey(Publicacion, on_delete = models.CASCADE)
+    publicacion = models.OneToOneField(Publicacion, on_delete = models.CASCADE, null = False, primary_key = True)
 
-class Libro(models.model):
-    editorial = models.CharField(null = True)
+class Libro(models.Model):
+    editorial = models.CharField(null = True, max_length = 45)
+    publicacion = models.OneToOneField(Publicacion, on_delete = models.CASCADE, null = False, primary_key = True)
+
+class Persona(models.Model):
+    id_persona = models.IntegerField(primary_key = True, null = False)
+    nombre = models.CharField(null = True, max_length = 45)
+    apellidos = models.CharField(null = True, max_length = 45)
+
+class PersonaPublicacion(models.Model):
+    persona = models.ForeignKey(Persona, on_delete = models.CASCADE, null = False)
+    publicacion = models.ForeignKey(Publicacion, on_delete = models.CASCADE, null = False)
