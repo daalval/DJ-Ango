f28915dc38e9b021cd58ec5a9e5d4934d94bdd90
Daniel Álvarez Vallejo <daalval@protonmail.com>
2020-11-25 19:11:51 +0100

    extractores de books y conferences hechos de ieee

--- bibliosearch/bibliosearch/controllers/ieeeEngine.py
+++ bibliosearch/bibliosearch/controllers/ieeeEngine.py
@@ -1,14 +1,21 @@
 import json
 import requests
 
-def fromJson(url, file):
+def fromJsonBooks(url, file):
+    data = requests.get(url, verify=False).json()
+
+    with open(file, 'w') as outfile:
+        json.dump(data, outfile)
+
+def fromJsonConferences(url, file):
     data = requests.get(url, verify=False).json()
 
     with open(file, 'w') as outfile:
         json.dump(data, outfile)
 
 def main():
-    fromJson("https://ieeexploreapi.ieee.org/api/v1/search/articles?parameter&apikey=efv84mzqq6ydx4dbd59jhdcn&content_type=Books", 'static/ieeeXplore.json')
+    fromJsonBooks("https://ieeexploreapi.ieee.org/api/v1/search/articles?parameter&apikey=efv84mzqq6ydx4dbd59jhdcn&content_type=Books", 'static/ieeeXploreBooks.json')
+    fromJsonConferences("https://ieeexploreapi.ieee.org/api/v1/search/articles?parameter&apikey=efv84mzqq6ydx4dbd59jhdcn&content_type=Conferences", 'static/ieeeXploreConferences.json')
 
 if __name__ == "__main__":
     main()
