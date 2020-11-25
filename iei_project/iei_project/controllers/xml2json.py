6a2b553ef8c0bf7abd8a09dc4abcc42ee11e4ea5
marferp2 <marferp2@inf.upv.es>
2020-11-25 22:30:00 +0100

    xml2json - DBLP wrapper done

--- iei_project/iei_project/controllers/xml2json.py
+++ iei_project/iei_project/controllers/xml2json.py
@@ -1,80 +1,120 @@
+import collections
 import json
+from typing import OrderedDict
 import xmltodict
+
 
 def xml_parser(xml_file, json_file):
     """Parser from XML to JSON / IEI"""
-    with open(xml_file) as xml_f: 
-        data_dict = xmltodict.parse(xml_f.read()) 
-        xml_f.close() 
+    with open(xml_file) as xml_f:
+        data_dict = xmltodict.parse(xml_f.read())
+        xml_f.close()
 
-    KEY='@key'
-    TITULO='title'
-    ANO='year'
-    URL='ee'
-    PAGINA_INICIO='pages'
-    PAGINA_FIN='pages'
-    VOLUMEN='volume'
-    NUMBER='number'
-    NOMBRE='journal'
-      
-    json_data = json.dumps(data_dict) 
-    articles=[]
+    KEY = '@key'
+    AUTOR = 'author'
+    TITULO = 'title'
+    ANO = 'year'
+    URL = 'ee'
+    PAGINA_INICIO = 'pages'
+    PAGINA_FIN = 'pages'
+    VOLUMEN = 'volume'
+    NUMBER = 'number'
+    NOMBRE = 'journal'
+
+    articles = {}
     for article_dict in data_dict['dblp']['article']:
         new_dict = {}
-        #key
-        new_dict['key'] = article_dict[KEY]
-        #titulo
+        # key
+        key = article_dict[KEY]
+        # titulo
         new_dict['titulo'] = article_dict[TITULO]
-        #list escritores
+
+        # list escritores
         escrita_por = []
-        for esc in article_dict['author']:
-            nom_ap = esc.split()
-            nombre = nom_ap[0]
-            apellidos = ''
-            for ap in nom_ap[1:len(nom_ap)]:
-                apellidos += f'{ap} '
-            escrita_por.append({nombre, apellidos})
+        if AUTOR in list(article_dict.keys()):
+            aut = article_dict[AUTOR]
+            if type(aut) == type(collections.OrderedDict()):
+                nom_ap = aut['#text'].split()
+                nombre = nom_ap[0]
+                apellidos = ''
+                for ap in nom_ap[1:len(nom_ap)]:
+                    apellidos += f'{ap} '
+                escrita_por.append({'nombre': nombre, 'apellidos': apellidos})
+            if type(aut) == type([]):
+                for esc in aut:
+                    if type(esc) == type(collections.OrderedDict()):
+                        nom_ap = esc['#text'].split()
+                        nombre = nom_ap[0]
+                        apellidos = ''
+                        for ap in nom_ap[1:len(nom_ap)]:
+                            apellidos += f'{ap} '
+                        escrita_por.append({'nombre': nombre, 'apellidos': apellidos})
+                    if type(esc) == type(''):
+                        nom_ap = esc.split()
+                        nombre = nom_ap[0]
+                        apellidos = ''
+                        for ap in nom_ap[1:len(nom_ap)]:
+                            apellidos += f'{ap} '
+                        escrita_por.append({'nombre': nombre, 'apellidos': apellidos})
+            if type(aut) == type(""):
+                nom_ap = aut.split()
+                nombre = nom_ap[0]
+                apellidos = ''
+                for ap in nom_ap[1:len(nom_ap)]:
+                    apellidos += f'{ap} '
+                escrita_por.append({'nombre': nombre, 'apellidos': apellidos})
 
         new_dict['escrita_por'] = escrita_por
-        #ano
+        # ano
         new_dict['ano'] = article_dict[ANO]
-        #url
-        new_dict['URL'] = article_dict[URL]
-        #pagina_inicio
-        new_dict['pagina_inicio'] = article_dict[PAGINA_INICIO].split('-')[0]
-        #pagina_fin
-        new_dict['pagina_fin'] = article_dict[PAGINA_FIN].split('-')[1]
-        #publicado_en.volumen
+        # url
+        if URL in list(article_dict.keys()):
+            new_dict['URL'] = article_dict[URL]
+
+        if (PAGINA_FIN) in list(article_dict.keys()):
+            p = article_dict[PAGINA_INICIO].split('-')
+            # pagina_inicio
+            new_dict['pagina_inicio'] = p[0]
+            # pagina_fin
+            if(len(p)>1):
+                new_dict['pagina_fin'] = article_dict[PAGINA_FIN].split('-')[1]
+
         publicado_en = {}
-        publicado_en['volumen'] = article_dict[VOLUMEN]
-        #publicado_en.numero
-        publicado_en['numero'] = article_dict[NUMBER]
-        #publicado_en.mes
+        if (VOLUMEN) in list(article_dict.keys()):
+            # publicado_en.volumen
+            publicado_en['volumen'] = article_dict[VOLUMEN]
+        if (NUMBER) in list(article_dict.keys()):
+            # publicado_en.numero
+            publicado_en['numero'] = article_dict[NUMBER]
+        # publicado_en.mes
         publicado_en['mes'] = None
         new_dict['publicado_en'] = publicado_en
-        #Ejemplar.tiene.nombre
-        Ejemplar = {
+        # Ejemplar.tiene.nombre
+        ejemplar = {
             'nombre': article_dict[NOMBRE]
         }
-        new_dict['Ejemplar'] = Ejemplar
-        #Revista.nombre
-        Revista = {
+        new_dict['ejemplar'] = ejemplar
+        # Revista.nombre
+        revista = {
             'nombre': article_dict[NOMBRE]
         }
-        new_dict['Revista'] = Revista
+        new_dict['revista'] = revista
+        articles[key] = new_dict
+
+    json_data = json.dumps(articles, indent=4)
     
-      
-    with open(json_file, "w") as json_file_opened: 
-        json_file_opened.write(json_data) 
+
+    with open(json_file, "w") as json_file_opened:
+        json_file_opened.write(json_data)
         json_file_opened.close()
+
 
 def main():
     """
     Main IEI parser
     """
-    xml_parser("../../static/dblp-pruebas.xml", "../../static/dblp.json")
+    xml_parser("../../static/DBLP-SOLO_ARTICLE.XML", "../../static/dblp.json")
 
 
 if __name__ == "__main__":
     main()
-
