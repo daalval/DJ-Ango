8f56f567692e801ec396771f8c4ecca70e3edb53
Daniel Álvarez Vallejo <daalval@protonmail.com>
2020-11-25 19:40:03 +0100

    hecho el de journals (no sé si son los articles)

--- bibliosearch/bibliosearch/controllers/ieeeEngine.py
+++ bibliosearch/bibliosearch/controllers/ieeeEngine.py
@@ -13,9 +13,16 @@ def fromJsonConferences(url, file):
     with open(file, 'w') as outfile:
         json.dump(data, outfile)
 
+def fromJsonJournals(url, file):
+    data = requests.get(url, verify=False).json()
+
+    with open(file, 'w') as outfile:
+        json.dump(data, outfile)
+
 def main():
     fromJsonBooks("https://ieeexploreapi.ieee.org/api/v1/search/articles?parameter&apikey=efv84mzqq6ydx4dbd59jhdcn&content_type=Books&max_records=200", 'static/ieeeXploreBooks.json')
     fromJsonConferences("https://ieeexploreapi.ieee.org/api/v1/search/articles?parameter&apikey=efv84mzqq6ydx4dbd59jhdcn&content_type=Conferences&max_records=200", 'static/ieeeXploreConferences.json')
+    fromJsonJournals("https://ieeexploreapi.ieee.org/api/v1/search/articles?parameter&apikey=efv84mzqq6ydx4dbd59jhdcn&content_type=Journals&max_records=200", 'static/ieeeXploreJournals.json')
 
 if __name__ == "__main__":
     main()
