286d674a2eb7a18d9373431426c4da9ed7d3c17e
daalval <daalval@inf.upv.es>
2020-11-26 09:52:38 +0100

    cosillas

--- bibliosearch/bibliosearch/controllers/ieeeEngine.py
+++ bibliosearch/bibliosearch/controllers/ieeeEngine.py
@@ -1,21 +1,20 @@
 import json
 import requests
 
-def fromJson(url, file):
+def fromJson(url, file, params):
     data = requests.get(url, verify=False, params = params).json()
 
     with open(file, 'w') as outfile:
         json.dump(data, outfile)
 
-params = dict(
-    content_type = 'Conferences',
-    max_records = '200',
-    start_year = '2010',
-    end_year = '2010'
-)
-
 def main():
-    fromJson("https://ieeexploreapi.ieee.org/api/v1/search/articles?parameter&apikey=efv84mzqq6ydx4dbd59jhdcn", 'static/ieeeXplore.json')
+    params = dict(
+        content_type = 'Conferences',
+        max_records = '200',
+        start_year = '2005',
+        end_year = '2010'
+    )
+    fromJson("https://ieeexploreapi.ieee.org/api/v1/search/articles?parameter&apikey=efv84mzqq6ydx4dbd59jhdcn", 'static/ieeeXplore.json', params)
 
 if __name__ == "__main__":
     main()
