154452d5feac4d24d011524161e85db9d9d223ea
daalval <daalval@inf.upv.es>
2020-11-26 14:00:33 +0100

    cosas

--- bibliosearch/bibliosearch/controllers/ieeeEngine.py
+++ bibliosearch/bibliosearch/controllers/ieeeEngine.py
@@ -1,25 +1,51 @@
 import json
 import requests
 
-def fromJson(url, file, params):
-    data = requests.get(url, verify=False, params = params).json()
+def query(url, file, our_content_types, our_start_year, our_end_year):
+    params = dict(
+        content_types = our_content_types,
+        start_year = our_start_year,
+        end_year = our_end_year,
+        max_records = '200',
+    )
 
-    for article in data['articles']:
-        print(article['title'])
-        print(article['rank'])
+    if len(our_content_types) == 1:
+        params = dict(
+            content_type = our_content_types[0],
+            start_year = our_start_year,
+            end_year = our_end_year,
+            max_records = '200',
+        )
+        data = requests.get(url, verify=False, params=params).json()
+        with open(file, 'w') as outfile:
+            json.dump(data, outfile)
+    else:
+        for type in our_content_types:
+            params = dict(
+                content_type = type,
+                start_year = our_start_year,
+                end_year = our_end_year,
+                max_records = '200',
+            )
+            data = requests.get(url, verify=False, params=params).json()
+            with open(file, 'a+') as outfile:
+                json.dump(data, outfile)
+    
     
 
-    with open(file, 'w') as outfile:
-        json.dump(data, outfile)
-
 def main():
-    params = dict(
-        content_type = 'Conferences',
-        max_records = '200',
-        start_year = '2005',
-        end_year = '2010'
-    )
-    fromJson("https://ieeexploreapi.ieee.org/api/v1/search/articles?parameter&apikey=efv84mzqq6ydx4dbd59jhdcn", 'static/ieeeXplore.json', params)
+    query("https://ieeexploreapi.ieee.org/api/v1/search/articles?parameter&apikey=efv84mzqq6ydx4dbd59jhdcn", 'static/ieeeXplore.json', ['Books', 'Conferences'], '2010', '2015')
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+# for article in data['articles']:
+#         print(article['title'])
+#         print(article['rank'])
