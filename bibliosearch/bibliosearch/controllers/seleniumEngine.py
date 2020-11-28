121f8d796a30e8bbd0c25c5f376a43f370b4829a
danihp6 <58531142+danihp6@users.noreply.github.com>
2020-11-28 13:14:38 +0100

    Bugs areglados

--- bibliosearch/bibliosearch/controllers/seleniumEngine.py
+++ bibliosearch/bibliosearch/controllers/seleniumEngine.py
@@ -13,60 +13,114 @@ INPROCEEDINGS = 'inproceedings'
 
 
 class Selenium(object):
-    def search(self, fecha_inicial, fecha_final, types):
-        driver = webdriver.Chrome('bibliosearch/controllers/chromedriver.exe')
-        driver.get("https://scholar.google.es/#d=gs_asd")
-        fecha_inicial_element = WebDriverWait(driver, 10).until(
-            lambda driver: driver.find_element_by_id("gs_asd_ylo"))
-        fecha_inicial_element.send_keys(fecha_inicial)
-        fecha_final_element = driver.find_element_by_id("gs_asd_yhi")
-        fecha_final_element.send_keys(fecha_final)
-        fecha_final_element.send_keys(Keys.RETURN)
-
-        listElements = driver.find_elements_by_class_name("gs_or_cit.gs_nph")
-
-        if len(listElements) == 0:
-            return {}
-
+    def search(self, fecha_inicial, fecha_final, autor, tipos):
         result = {}
-
-        index = 0
-
-        # while(index < len(listElements)):
-        #     result.update(self.extract_element(
-        #         listElements[index], driver, types))
-        #     index = index + 1
-        #     listElements = WebDriverWait(driver, 10).until(
-        #         lambda driver: driver.find_elements_by_class_name("gs_or_cit.gs_nph"))
-
         try:
-            next_page = driver.find_element_by_xpath(
-                '//*[@id="gs_nm"]/button[2]')
+            driver = webdriver.Chrome(
+                'bibliosearch/controllers/chromedriver.exe')
+            driver.get('https://scholar.google.es/#d=gs_asd')
+            author_element = WebDriverWait(driver, 10).until(
+                lambda driver: driver.find_element_by_id('gs_asd_sau'))
+            author_element.send_keys(autor)
+            fecha_inicial_element = WebDriverWait(driver, 10).until(
+                lambda driver: driver.find_element_by_id('gs_asd_ylo'))
+            fecha_inicial_element.send_keys(fecha_inicial)
+            fecha_final_element = driver.find_element_by_id('gs_asd_yhi')
+            fecha_final_element.send_keys(fecha_final)
+            fecha_final_element.send_keys(Keys.RETURN)
 
-            while next_page.is_enabled():
-                next_page.click()
-                listElements = WebDriverWait(driver, 10).until(
-                    lambda driver: driver.find_elements_by_class_name("gs_or_cit.gs_nph"))
-                index = 0
-                # while(index < len(listElements)):
-                #     result.update(self.extract_element(
-                #         listElements[index], driver, types))
-                #     index = index + 1
-                #     listElements = WebDriverWait(driver, 10).until(
-                #         lambda driver: driver.find_elements_by_class_name("gs_or_cit.gs_nph"))
+            listElements = driver.find_elements_by_class_name(
+                'gs_or_cit.gs_nph')
+
+            if len(listElements) == 0:
+                return result
+
+            index = 0
+
+            while(index < len(listElements)):
                 result.update(self.extract_element(
-                    listElements[3], driver, types))
+                    listElements[index], driver, tipos))
+                index = index + 1
+                listElements = WebDriverWait(driver, 10).until(
+                    lambda driver: driver.find_elements_by_class_name('gs_or_cit.gs_nph'))
+
+            mas_paginas = driver.find_element_by_xpath(
+                '/html/body/div/div[9]/div[3]/div').get_attribute('innerText')
+
+            if mas_paginas[0] == 'P':
                 next_page = driver.find_element_by_xpath(
                     '//*[@id="gs_nm"]/button[2]')
+
+                while next_page.is_enabled():
+                    next_page.click()
+                    listElements = WebDriverWait(driver, 10).until(
+                        lambda driver: driver.find_elements_by_class_name('gs_or_cit.gs_nph'))
+                    index = 0
+                    while(index < len(listElements)):
+                        result.update(self.extract_element(
+                            listElements[index], driver, tipos))
+                        index = index + 1
+                        listElements = WebDriverWait(driver, 10).until(
+                            lambda driver: driver.find_elements_by_class_name('gs_or_cit.gs_nph'))
+                    next_page = driver.find_element_by_xpath(
+                        '//*[@id="gs_nm"]/button[2]')
+
         except Exception as e:
             print(e)
+            return result
 
         return result
 
-    def extract_element(self, element, driver, types):
+    def search_only_first_element(self, fecha_inicial, fecha_final, tipos):
+        result = {}
+        try:
+            driver = webdriver.Chrome(
+                'bibliosearch/controllers/chromedriver.exe')
+            driver.get('https://scholar.google.es/#d=gs_asd')
+            fecha_inicial_element = WebDriverWait(driver, 10).until(
+                lambda driver: driver.find_element_by_id('gs_asd_ylo'))
+            fecha_inicial_element.send_keys(fecha_inicial)
+            fecha_final_element = driver.find_element_by_id('gs_asd_yhi')
+            fecha_final_element.send_keys(fecha_final)
+            fecha_final_element.send_keys(Keys.RETURN)
+
+            listElements = driver.find_elements_by_class_name(
+                'gs_or_cit.gs_nph')
+
+            if len(listElements) == 0:
+                return result
+
+            result.update(self.extract_element(
+                listElements[0], driver, tipos))
+
+            mas_paginas = driver.find_element_by_xpath(
+                '/html/body/div/div[9]/div[3]/div').get_attribute('innerText')
+
+            print(mas_paginas)
+
+            if mas_paginas[0] == 'P' or mas_paginas[0] == 'A':
+                next_page = driver.find_element_by_xpath(
+                    '//*[@id="gs_nm"]/button[2]')
+
+                while next_page.is_enabled():
+                    next_page.click()
+                    listElements = WebDriverWait(driver, 10).until(
+                        lambda driver: driver.find_elements_by_class_name('gs_or_cit.gs_nph'))
+                    result.update(self.extract_element(
+                        listElements[0], driver, tipos))
+                    next_page = driver.find_element_by_xpath(
+                        '//*[@id="gs_nm"]/button[2]')
+
+        except Exception as e:
+            print(e)
+            return result
+
+        return result
+
+    def extract_element(self, element, driver, tipos):
         element.click()
         WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.CLASS_NAME, "gs_citi"))).click()
+            EC.element_to_be_clickable((By.CLASS_NAME, 'gs_citi'))).click()
         text = driver.find_element_by_tag_name(
             'pre').get_attribute('innerText')
 
@@ -78,17 +132,18 @@ class Selenium(object):
 
         key = entry.key
 
-        if type not in types:
+        if type not in tipos:
             driver.back()
             driver.back()
             return {}
 
         fields = entry.fields
-        title = self.fix_string(fields.get('title'))
-        print(title)
+        title = self.fix_string_without_accent(fields.get('title'))
+
         authors = entry.persons.get('author')
         year = fields.get('year')
-        publisher = fields.get('publisher') or fields.get('journal')
+        publisher = self.fix_string_without_accent(fields.get(
+            'publisher')) or self.fix_string_without_accent(fields.get('journal'))
 
         personas = []
 
@@ -101,9 +156,8 @@ class Selenium(object):
         result = {
 
             'ano': year,
-            'url': None,
             'escrita_por': personas,
-            'titulo': title
+            'titulo': title,
         }
 
         # --------------------------------------------ARTICULOS------------------------------------------
@@ -126,10 +180,10 @@ class Selenium(object):
             if pages != None:
                 pages = pages.split('--')
                 initial_page = pages[0]
-                final_page = pages[1]
+                if len(pages) > 1:
+                    final_page = pages[1]
             result.update({
                 'tipo': 'articulo',
-                'titulo': title,
                 'pagina_inicio': initial_page,
                 'pagina_final': final_page,
                 'publicado_en': publicado_en
@@ -139,26 +193,26 @@ class Selenium(object):
             pages = fields.get('pages')
             result.update({
                 'tipo': 'libro',
-                'titulo': title,
                 'paginas': pages,
                 'editorial': publisher
             })
 
         # --------------------------------------------COMUNICACION-CONGRESO------------------------------------------
         if type == INPROCEEDINGS:
-            organization = fields.get('organization')
+            organization = self.fix_string_without_accent(
+                fields.get('organization'))
             pages = fields.get('pages')
             initial_page = None
             final_page = None
             if pages != None:
                 pages = pages.split('--')
                 initial_page = pages[0]
-                final_page = pages[1]
+                if len(pages) > 1:
+                    final_page = pages[1]
             place = None
 
             result.update({
                 'tipo': 'con_con',
-                'titulo': title,
                 'pagina_inicio': initial_page,
                 'pagina_final': final_page,
                 'editorial': publisher,
@@ -169,50 +223,56 @@ class Selenium(object):
         driver.back()
         driver.back()
 
-        url = driver.find_element_by_xpath(
-            f"//*[text()='{title}']").get_attribute('href')
-        result.update({'url': url})
+        url = None
+
+        try:
+            url = driver.find_element_by_xpath(
+            '/html/body/div/div[10]/div[2]/div[2]/div[2]/div[4]/div/h3/a')
+            
+            result.update({'url': url.get_attribute('href')})
+        except:
+            pass
+        if url == None :
+            try:
+                url = driver.find_element_by_xpath(
+                '/html/body/div/div[10]/div[2]/div[2]/div[2]/div[4]/div[2]/h3/a')
+
+                result.update({'url': url.get_attribute('href')})
+            except:
+                pass
         return {
             key: result
         }
 
-    def fix_string(self,string):
-        index = string.find("{\\")
-        print(index)
+    def fix_string_without_accent(self, string):
+        if string == None:
+            return None
+        index = string.find('{\\')
         while index != -1:
-            char =self.put_accent(string[index + 4])
+            charIndex = 0
+
+            if string[index + 3] == '\\':
+                charIndex = 4
+            else:
+                charIndex = 3
+
+            char = string[index + charIndex]
             pre = string[0:index]
-            post = string[index + (6, 4)[char == 'ñ' or char == 'Ñ']: len(string)]
+            post = string[index + charIndex + 2: len(string)]
             string = pre + char + post
             index = string.find('{\\\'')
         return string
 
-    def put_accent(self, char):
-        switcher = {
-            'o': 'ó',
-            'O': 'Ó',
-            'n': 'ñ',
-            'N': 'Ñ',
-            'i': 'í',
-            'I': 'Í',
-            'a': 'á',
-            'A': 'Á',
-            'e': 'é',
-            'E': 'É',
-            'u': 'ú',
-            'U': "Ú"
-        }
-        return switcher[char]
-
 
 def main():
     selenium = Selenium()
-    result = selenium.search('1000', '1800', [BOOK])
+    result = selenium.search(
+        '1000', '1500', '', [ARTICLE, BOOK, INPROCEEDINGS])
 
     with open('results/google_schoolar.json', 'w') as json_file:
         json.dump(result, json_file)
     json_file.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
