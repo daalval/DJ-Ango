b88d159272e0eebb181e3ce1fd12b9068a940cf7
Daniel Álvarez Vallejo <daalval@protonmail.com>
2021-01-07 21:50:29 +0100

    despliegue

--- bibliosearch/settings.py
+++ bibliosearch/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["sheltered-taiga-55085.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["bibliosearch.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
