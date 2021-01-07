446391b71228e8b0a853cfd866f0b290218fc8fc
Daniel Álvarez Vallejo <daalval@protonmail.com>
2021-01-07 21:14:20 +0100

    despliegue

--- bibliosearch/settings.py
+++ bibliosearch/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -90,6 +91,9 @@ DATABASES = {
         'DATABASE_PORT': '5432'
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
