6df582676d7d3937d75ed0de833ae533fac67f80
daalval <daalval@inf.upv.es>
2021-01-08 10:22:55 +0100

    nada

--- bibliosearch/settings.py
+++ bibliosearch/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'bibliosearch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bibliosearch',
+        'USER': 'postgres',
+        'PASSWORD': 'diyeiango',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432'
     }
 }
 
