119494c0bb7f212a07798572fe7927a4affab3ca
daalval <daalval@inf.upv.es>
2021-01-08 01:51:03 +0100

    nada

--- bbdd/models.py
+++ bbdd/models.py
@@ -18,7 +18,7 @@ class Ejemplar(models.Model):
 
 class Publicacion(models.Model):
     id_publicacion = models.IntegerField(primary_key = True, null = False, unique=True)
-    titulo = models.CharField(null = True, max_length = 45)
+    titulo = models.CharField(null = True, max_length = 45, unique=True)
     anyo = models.IntegerField(null = True)
     URL = models.CharField(null = True, max_length = 45)
 
