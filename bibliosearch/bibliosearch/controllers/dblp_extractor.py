d95adcf3aab18ca52de04d3227f66f8f5aba96ef
marferp2 <marferp2@inf.upv.es>
2021-01-10 19:34:48 +0100

    Update dblp_extractor.py

--- bibliosearch/bibliosearch/controllers/dblp_extractor.py
+++ bibliosearch/bibliosearch/controllers/dblp_extractor.py
@@ -3,13 +3,16 @@ import json
 from typing import OrderedDict
 import xmltodict
 
-
-def xml_parser(xml_file, json_file, year_st, year_end):
+# Este método recorre el archivo dado en el parámetro "xml_file",
+# seleccionando los artículos que van desde el año "year_st" hasta el "year_end"
+def xml_parser(xml_file, year_st, year_end):
     """Parser from XML to JSON / IEI"""
     with open(xml_file) as xml_f:
         data_dict = xmltodict.parse(xml_f.read())
         xml_f.close()
 
+
+    #Constantes de los atributos xml
     KEY = '@key'
     AUTOR = 'author'
     TITULO = 'title'
@@ -24,19 +27,26 @@ def xml_parser(xml_file, json_file, year_st, year_end):
     articles = {}
     for article_dict in data_dict['dblp']['article']:
         new_dict = {}
-        # ano
+
+        # Comprobación del año
         if int(article_dict[ANO])>=year_st and int(article_dict[ANO])<=year_end:
             new_dict['anyo'] = str(article_dict[ANO])
-            # key
+
+            # Key
             key = article_dict[KEY]
+
+            # Tipo de publicación
             new_dict['tipo'] = "articulo"
-            # titulo
+
+            # Titulo
             new_dict['titulo'] = article_dict[TITULO]
 
-            # list escritores
+            # Escritores
             escrita_por = []
             if AUTOR in list(article_dict.keys()):
                 aut = article_dict[AUTOR]
+
+                # Si el atributo es un OrderedDict
                 if type(aut) == type(collections.OrderedDict()):
                     nom_ap = aut['#text'].split()
                     nombre = nom_ap[0]
@@ -44,6 +54,8 @@ def xml_parser(xml_file, json_file, year_st, year_end):
                     for ap in nom_ap[1:len(nom_ap)]:
                         apellidos += f'{ap} '
                     escrita_por.append({'nombre': nombre, 'apellidos': apellidos})
+
+                # Si el atributo es una lista
                 if type(aut) == type([]):
                     for esc in aut:
                         if type(esc) == type(collections.OrderedDict()):
@@ -60,6 +72,8 @@ def xml_parser(xml_file, json_file, year_st, year_end):
                             for ap in nom_ap[1:len(nom_ap)]:
                                 apellidos += f'{ap} '
                             escrita_por.append({'nombre': nombre, 'apellidos': apellidos})
+
+                # Si el atributo es un String
                 if type(aut) == type(""):
                     nom_ap = aut.split()
                     nombre = nom_ap[0]
@@ -70,8 +84,7 @@ def xml_parser(xml_file, json_file, year_st, year_end):
             
             new_dict['escrita_por'] = escrita_por
 
-
-            # url
+            # Url
             añadir_url = ""
             if URL in list(article_dict.keys()):
                 url = article_dict[URL]
@@ -84,10 +97,13 @@ def xml_parser(xml_file, json_file, year_st, year_end):
             
             new_dict['url'] = añadir_url
 
+            # Páginas
             if (PAGINA_FIN) in list(article_dict.keys()):
                 p = article_dict[PAGINA_INICIO].split('-')
+
                 # pagina_inicio
                 new_dict['pagina_inicio'] = p[0]
+
                 # pagina_fin
                 if(len(p)>1):
                     new_dict['pagina_fin'] = article_dict[PAGINA_FIN].split('-')[1]
@@ -97,48 +113,41 @@ def xml_parser(xml_file, json_file, year_st, year_end):
                 new_dict['pagina_inicio'] = None
                 new_dict['pagina_fin'] = None
 
+            # Publicado_en
             publicado_en = {}
+
+            # publicado_en.volumen
             if (VOLUMEN) in list(article_dict.keys()):
-                # publicado_en.volumen
                 publicado_en['volumen'] = article_dict[VOLUMEN]
             else:
                 publicado_en['volumen'] = None
+            
+            # publicado_en.numero
             if (NUMBER) in list(article_dict.keys()):
-                # publicado_en.numero
                 publicado_en['numero'] = article_dict[NUMBER]
             else:
                 publicado_en['numero'] = None
+
             # publicado_en.mes
             publicado_en['mes'] = None
+
             # Revista.nombre
             revista = {
                 'nombre': article_dict[NOMBRE]
             }
             publicado_en['revista'] = revista
             new_dict['publicado_en'] = publicado_en
-            # Ejemplar.tiene.nombre
-            #ejemplar = {
-            #    'nombre': article_dict[NOMBRE]
-            #}
-            #new_dict['ejemplar'] = ejemplar
-            # Revista.nombre
+
             articles[key] = new_dict
 
     return articles
-
-    # json_data = json.dumps(articles, indent=4)
-    
-
-    # with open(json_file, "w") as json_file_opened:
-    #     json_file_opened.write(json_data)
-    #     json_file_opened.close()
 
 
 def main():
     """
     Main IEI parser
     """
-    result = xml_parser("static/dblp-pruebas.xml", "static/dblp.json", 2005, 2020)
+    result = xml_parser("static/dblp-pruebas.xml", 2005, 2020)
 
     with open('static/dblp.json', 'w') as json_file:
         json.dump(result, json_file)
